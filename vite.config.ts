8c6754a4faf1129063b86c5a2d0e5f5665094b7b
someonempty <someonempty@gmail.com>
2024-03-08 20:25:02 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es6',
-    lib: {
-      entry: 'src/main.tsx',
-      formats: ['cjs']
-    }
-  }
 })
